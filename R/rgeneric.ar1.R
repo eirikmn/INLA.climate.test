0250c3f6236a5253ab703eec2da72943a78a5d8a
Eirik Myrvoll-Nilsen <eirikmn91@gmail.com>
2020-01-08 13:47:30 +0100

    Trying something

--- R/rgeneric.ar1.R
+++ R/rgeneric.ar1.R
@@ -24,6 +24,15 @@ rgeneric.ar1 = function(
     denom = sum(exp(c(0,theta[3+(1:(NN-1))])))
     
     weights=numeric(NN)
+    
+    if(NN==2){
+      para$w1 <- theta[4]
+      para$w2 <- 1-para$w1
+      para$p1 <- theta[5]
+      para$p2 <- theta[6]
+      return(para)
+    }
+    
     if(NN>1){
       for(i in 2:(NN)){
         weights[i] <- exp(theta[2+i])
@@ -210,6 +219,13 @@ rgeneric.ar1 = function(
     #lprior = lprior + dnorm(theta[4],log=TRUE)
     lprior = lprior + dnorm(theta[3],log=TRUE)
     #lprior = lprior + dnorm(-shift.a+2*shift.a/(1+exp(-params$F0)),sd=0.2,log=TRUE)+log(2*shift.a)-params$F0 -2*log(1+exp(-params$F0))
+    if(NN==2){
+      lprior = lprior + inla.pc.dcor0(params$w1,0.5,0.5)
+      lprior = lprior + dbeta(params$p1,3,3)
+      lprior = lprior + dbeta(params$p2,3,3)
+      
+      return(lprior)
+    }
     if(NN==1){
       lprior = lprior + dnorm(theta[4],log=TRUE)
       return(lprior)
